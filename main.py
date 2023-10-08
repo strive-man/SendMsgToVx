ae4feda71106129b2e0abf4841eed5b9729d6c6e
E.M.H <92583611+strive-man@users.noreply.github.com>
2023-10-08 22:25:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import math
 import random
 import requests
-
 from datetime import date, datetime
 from wechatpy import WeChatClient
 from wechatpy.client.api import WeChatMessage, WeChatTemplate
@@ -45,21 +44,14 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 
 for i in range(len(user_ids)):
-    wea, tem = get_weather(citys[i])
-    cit, dat = get_city_date(citys[i])
+    wea, tem = get_weather(city[i])
+    cit, dat = get_city_date(city[i])
     data = {
         "date": {"value": "今日日期：{}".format(dat), "color": get_random_color()},
         "city": {"value": "当前城市：{}".format(cit), "color": get_random_color()},
         "weather": {"value": "今日天气：{}".format(wea), "color": get_random_color()},
         "temperature": {"value": "当前温度：{}".format(tem), "color": get_random_color()},
-        "love_days": {"value": "今天是你们在一起的第{}天".format(get_count(start_dates[i])), "color": get_random_color()},
-        "birthday_left": {"value": "距离她的生日还有{}天".format(get_birthday(birthdays[i])), "color": get_random_color()},
-        "solary": {"value": "距离发工资还有{}天".format(get_solary(solarys[i])), "color": get_random_color()},
         "words": {"value": get_words(), "color": get_random_color()}
     }
-    if get_birthday(birthdays[i]) == 0:
-        data["birthday_left"]['value'] = "今天是她的生日哦，快去一起甜蜜吧"
-    if get_solary(solarys[i]) == 0:
-        data["solary"]['value'] = "今天发工资啦，快去犒劳一下自己吧"
     res = wm.send_template(user_ids[i], template_ids[i], data)
     print(res)
