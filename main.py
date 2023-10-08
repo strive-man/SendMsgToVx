753a051ef8c27b3ae1e79a7e2d5073bf5be2e40d
E.M.H <92583611+strive-man@users.noreply.github.com>
2023-10-08 22:36:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,53 @@
-import os
-import math
-import random
-import requests
 from datetime import date, datetime
+import math
 from wechatpy import WeChatClient
 from wechatpy.client.api import WeChatMessage, WeChatTemplate
+import requests
+import os
+import random
 
 today = datetime.now()
+start_date = os.environ['START_DATE']
+city = os.environ['CITY']
+birthday = os.environ['BIRTHDAY']
 
-# 微信公众测试号ID和SECRET
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
-# 可把os.environ结果替换成字符串在本地调试
-user_ids = os.environ["USER_ID"].split(',')
-template_ids = os.environ["TEMPLATE_ID"].split(',')
-city = os.environ["CITY"].split(',')
+user_id = os.environ["USER_ID"]
+template_id = os.environ["TEMPLATE_ID"]
 
-# 获取天气和温度
+
 def get_weather():
-    url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-    res = requests.get(url).json()
-    weather = res['data']['list'][0]
-    return weather['weather'], math.floor(weather['temp'])
+  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+  res = requests.get(url).json()
+  weather = res['data']['list'][0]
+  return weather['weather'], math.floor(weather['temp'])
 
-# 当前城市、日期
-def get_city_date(city):
-    return city, today.date().strftime("%Y-%m-%d")
+def get_count():
+  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+  return delta.days
 
-# 每日一句
+def get_birthday():
+  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  if next < datetime.now():
+    next = next.replace(year=next.year + 1)
+  return (next - today).days
+
 def get_words():
-    words = requests.get("https://api.shadiao.pro/chp")
-    if words.status_code != 200:
-        return get_words()
-    return words.json()['data']['text']
+  words = requests.get("https://api.shadiao.pro/chp")
+  if words.status_code != 200:
+    return get_words()
+  return words.json()['data']['text']
 
-# 字体随机颜色
 def get_random_color():
-    return "#%06x" % random.randint(0, 0xFFFFFF)
+  return "#%06x" % random.randint(0, 0xFFFFFF)
 
 
 client = WeChatClient(app_id, app_secret)
-wm = WeChatMessage(client)
 
-for i in range(len(user_ids)):
-    wea, tem = get_weather(city[i])
-    cit, dat = get_city_date(city[i])
-    data = {
-        "date": {"value": "今日日期：{}".format(dat), "color": get_random_color()},
-        "city": {"value": "当前城市：{}".format(cit), "color": get_random_color()},
-        "weather": {"value": "今日天气：{}".format(wea), "color": get_random_color()},
-        "temperature": {"value": "当前温度：{}".format(tem), "color": get_random_color()},
-        "words": {"value": get_words(), "color": get_random_color()}
-    }
-    res = wm.send_template(user_ids[i], template_ids[i], data)
-    print(res)
+wm = WeChatMessage(client)
+wea, temperature = get_weather()
+data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+res = wm.send_template(user_id, template_id, data)
+print(res)
