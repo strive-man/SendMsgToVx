7824f4ae123039c864b9499fc562ff18f202ee61
E.M.H <92583611+strive-man@users.noreply.github.com>
2023-10-08 22:22:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,7 @@ app_secret = os.environ["APP_SECRET"]
 # 可把os.environ结果替换成字符串在本地调试
 user_ids = os.environ["USER_ID"].split(',')
 template_ids = os.environ["TEMPLATE_ID"].split(',')
-citys = os.environ["CITY"].split(',')
-
+city = os.environ["CITY"].split(',')
 
 # 获取天气和温度
 def get_weather(city):
@@ -26,35 +25,9 @@ def get_weather(city):
     weather = res['data']['list'][0]
     return weather['weather'], math.floor(weather['temp'])
 
-
 # 当前城市、日期
 def get_city_date(city):
     return city, today.date().strftime("%Y-%m-%d")
-
-
-# 距离设置的日期过了多少天
-def get_count(start_date):
-    delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-    return delta.days
-
-
-# 距离发工资还有多少天
-def get_solary(solary):
-    next = datetime.strptime(str(date.today().year) + "-" + str(date.today().month) + "-" + solary, "%Y-%m-%d")
-    if next < datetime.now():
-        if next.month == 12:
-            next = next.replace(year=next.year + 1)
-        next = next.replace(month=(next.month + 1) % 12)
-    return (next - today).days
-
-
-# 距离过生日还有多少天
-def get_birthday(birthday):
-    next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-    if next < datetime.now():
-        next = next.replace(year=next.year + 1)
-    return (next - today).days
-
 
 # 每日一句
 def get_words():
@@ -62,7 +35,6 @@ def get_words():
     if words.status_code != 200:
         return get_words()
     return words.json()['data']['text']
-
 
 # 字体随机颜色
 def get_random_color():
